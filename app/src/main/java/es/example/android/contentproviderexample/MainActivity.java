2ab2d763a24f5522795f61c8abd5da1a07c9a1de
Fer <pher.gm@gmail.com>
2014-11-30 00:13:00 +0100

    added ContentResolver

--- app/src/main/java/es/example/android/contentproviderexample/MainActivity.java
+++ app/src/main/java/es/example/android/contentproviderexample/MainActivity.java
@@ -1,6 +1,9 @@
 package es.example.android.contentproviderexample;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.UserDictionary;
 import android.support.v7.app.ActionBarActivity;
 
 
@@ -10,6 +13,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the ContentResolver which will send a message to the ContentProvider
+        ContentResolver resolver = getContentResolver();
+
+        // Get a Cursor containing all of the rows in the Words table
+        Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
+
     }
 
 }
