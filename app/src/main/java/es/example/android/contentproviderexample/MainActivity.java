b7987ef670873b18e1ba1afdd797fa083b4e9c73
Fer <pher.gm@gmail.com>
2014-11-30 00:39:13 +0100

    use cursor to insert the dictionary words into TextView

--- app/src/main/java/es/example/android/contentproviderexample/MainActivity.java
+++ app/src/main/java/es/example/android/contentproviderexample/MainActivity.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,12 +15,31 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView dictionaryWordsTextView = (TextView) findViewById(R.id.dictionaryWords);
+
         // Get the ContentResolver which will send a message to the ContentProvider
         ContentResolver resolver = getContentResolver();
 
         // Get a Cursor containing all of the rows in the Words table
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
 
+        try {
+            while(cursor.moveToNext()) {
+                int idColumn = cursor.getColumnIndex(UserDictionary.Words._ID);
+                int frequencyColumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
+                int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
+                int id = cursor.getInt(idColumn);
+                int frequency = cursor.getInt(frequencyColumn);
+                String word = cursor.getString(wordColumn);
+                dictionaryWordsTextView.append(("\n" + id + " - " + frequency + " - " + word));
+            }
+        } catch(Exception e){
+
+        }
+        finally {
+            // Always close your cursor to avoid memory leaks
+            cursor.close();
+        }
     }
 
 }
